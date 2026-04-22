2a8a8b5db2e1777b9b727daf357f324f0423fb73
truyennn.hgi <truyennn.hgi@vnpt.vn>
2026-04-22 07:51:27 +0700

    sua port và server

--- fe/src/services/api.js
+++ fe/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "~/store/auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://thitructuyen-demo-api/api",
     headers: {
         "Content-Type": "application/json",
     },
