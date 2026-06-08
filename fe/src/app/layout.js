578d45784b90d4e611219e43de216829f93d3f7c
truyennn.hgi <truyennn.hgi@vnpt.vn>
2026-06-08 15:07:21 +0700

    feat: update metadata structure and adjust language settings in RootLayout

--- fe/src/app/layout.js
+++ fe/src/app/layout.js
@@ -3,15 +3,18 @@ import "antd/dist/reset.css";
 import ClientLayout from "./ClientLayout";
 
 export const metadata = {
-    title: 'Thi trực tuyến'
+    title: 'Thi trực tuyến',
+    other: {
+        google: "notranslate",
+    },
 };
 
 export default function RootLayout({children}) {
 
 
     return (
-        <html lang="en">
-        <body className="min-h-screen bg-slate-50 p-0 m-0 overflow-x-hidden">
+        <html lang="vi" translate="no" className="notranslate">
+        <body className="notranslate min-h-screen bg-slate-50 p-0 m-0 overflow-x-hidden">
         <ClientLayout>
             {children}
         </ClientLayout>
